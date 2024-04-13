ad71f6396e4254c01e5774d31352502282ce23cf
RivalStellaMarias09 <163362493+RivalStellaMarias09@users.noreply.github.com>
2024-04-13 13:45:15 -0500

    Chapter 19 Homework

---
+++ main.js
@@ -0,0 +1,6 @@
+function functionthankyou(){
+    document.getElementById("thankyou").innerHTML="Thank You For Making A Donation";
+}
+function functionyouareloggedin(){
+    document.getElementById("youareloggedin").innerHTML="You Are Logged In";
+}
